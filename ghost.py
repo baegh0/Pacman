a102bee770caef2ed4d180f0ab885aeb0dff6d74
anguyen3140 <angnguyen@csu.fullerton.edu>
2022-10-12 21:07:13 -0700

    added images

---
+++ ghost.py
@@ -0,0 +1,18 @@
+import pygame as pg
+from vector import Vector
+
+class Ghost(self, screen):
+    def __init__(self): pass
+    def draw(self): pass
+
+    def update(self): pass
+    # Switch to chasing PacMan
+    def switch_chase(self): pass
+    # Switch to scatter, running away
+    def switch_scatter(self): pass
+    # Switch to running key
+    def switch_run(self): pass
+
+    def switch_spawn(self): pass
+    def choose_node(self): pass
+    def change_direction(self): pass
