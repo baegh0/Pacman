2dac8065fc50c411e38213162a3957024b6bf1c3
anguyen3140 <angnguyen@csu.fullerton.edu>
2022-10-23 12:48:50 -0700

    starting screen made and play button implemented

--- game.py
+++ game.py
@@ -12,34 +12,56 @@ class Game(object):
         pg.init()
         self.settings = setting
         self.screen = pg.display.set_mode(setting.SCREENSIZE, 0, 32)
+        self.bg_image = pg.image.load('images/bg.png')
         self.background = None
         self.clock = pg.time.Clock()
+        self.createComponents()
     
+    #creates the Starting Screen Menu
+    def startScreen(self):
+        while True:
+            self.screen.blit(self.bg_image, (0,0))
+            pg.display.flip()
+            self.check_button()
+
     def setBackground(self):
         self.background = pg.surface.Surface(setting.SCREENSIZE).convert()
         self.background.fill(setting.BLACK)
+
+    # Checks if the play button is pressed or the high score button
+    def check_button(self):
+        mouse = pg.mouse.get_pos()
+        ev = pg.event.get()
+        for event in ev:
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if 248 <= mouse[0] <= 452 and 568 <= mouse[1] <= 644:
+                 self.startGame()
+                # if 286 <= mouse[0] <= 515 and 438 <= mouse[1] <= 492:
+                #     pg.quit()
+                #     sys.exit()
+                # if 290 <= mouse[0] <= 515 and 372 <= mouse[1] <= 421:
+                #     self.hs_screen()
     
+    def createComponents(self):
+        self.nodes = Nodes("maze.txt")
+        self.nodes.setSides((0,17), (27,17))
+        self.player = Player(self.nodes.getStartNode())
+        self.candy = CandyGroup()
+
     def startGame(self):
         self.setBackground()
         self.nodes = Nodes("maze.txt")
         self.nodes.setSides((0,17), (27,17))
         self.player = Player(self.nodes.getStartNode())
         self.candy = CandyGroup()
-        # self.ghost = Ghost(self.nodes.getStartNode(), self.player) #getStartNode() has not been implemeneted in node.py yet
+        while True:
+            self.update()
 
-    
     def checkEvents(self):
         for event in pg.event.get():
             if event.type == QUIT:
                 pg.quit()
                 sys.exit()
-
-    # creates ghosts at locations on screen? 
-    # def createGhosts(self):
-    #     self.angel.rect.left, self.angel.rect.top = 336, 432
-    #     self.butterfly.rect.left, self.butterfly.rect.top = 336, 432
-    #     self.devil.rect.left, self.devil.rect.top = 336, 432
-    #     self.witch.rect.left, self.witch.rect.top = 336, 432
     
     def update(self):
         timer = self.clock.tick(30) / 1000
@@ -54,11 +76,9 @@ class Game(object):
         self.player.draw(self.screen)
         self.nodes.draw(self.screen)
         self.candy.draw(self.screen)
-        # self.ghost.draw(self.screen)
         pg.display.update()
 
 if __name__ == "__main__":
     game = Game()
-    game.startGame()
-    while True:
-        game.update()
+    game.startScreen()
+    # game.startGame()
